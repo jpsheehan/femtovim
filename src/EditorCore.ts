3436aaba57221c92f8575765b7513923298ac46e
Jesse Sheehan <jesse@sheehan.nz>
2022-01-22 15:25:30 +1300

    Added more tests for findEndOfWordForward

--- src/EditorCore.ts
+++ src/EditorCore.ts
@@ -1,36 +1,27 @@
 /**
- * Finds the next word within the string from some index.
+ * Finds the start of the next word within the string from some index.
  * If this is the last word in the string then the index does not change.
  * 
  * @param str The string.
  * @param idx The current cursor index in the string.
  * @returns The new cursor index.
  */
-function findNextWord(str: string, idx: number): number {
-    const strFromIdx = str.substring(idx);
-    const idxOfWhiteSpace = strFromIdx.search(/\s/);
-    if (idxOfWhiteSpace === -1) {
-        return idx;
-    }
-    const strFromWhiteSpace = strFromIdx.substring(idxOfWhiteSpace);
-    const idxOfNextWord = strFromWhiteSpace.search(/\w/);
-    if (idxOfNextWord === -1) {
-        return idx;
-    }
+function findStartOfWordForward(str: string, idx: number): number {
+
 
     return -1;
 }
 
 /**
- * Finds the end of the current or next word (whichever is closer).
+ * Finds the end of the current word (if the cursor is not at the end of the word) or the end of the next word.
  * If there are no other words in the buffer, then return `idx`.
  * 
  * @param str The string.
  * @param idx The current cursor index in the string.
  * @returns The new cursor index.
  */
-export function findEndOfWord(str: string, idx: number): number {
-    const strFromIdx = str.substring(idx);
+export function findEndOfWordForward(str: string, idx: number): number {
+    const strFromIdx = str.substring(idx + 1);
     if (strFromIdx.length === 0) {
         return idx;
     }
@@ -48,11 +39,6 @@ export function findEndOfWord(str: string, idx: number): number {
         return idx;
     }
 
-    // go forward one character as we didn't skip the current word
-    if (idxOfNextNonWord === 1) {
-        return findEndOfWord(str, idx + idxOfNextWord + 1);
-    }
-
     // go backward one character to get the last character in the word
-    return idx + idxOfNextWord + idxOfNextNonWord - 1;
+    return idx + idxOfNextWord + idxOfNextNonWord;
 }
--- test/test.ts
+++ test/test.ts
@@ -1,34 +1,42 @@
 import { describe, it } from "mocha"
 import { assert } from "chai";
-import { findEndOfWord } from "../src/EditorCore"
+import { findEndOfWordForward } from "../src/EditorCore"
 
 const exampleSentence = "The quick brown fox jumps over the lazy dog";
 const emptySentence = "";
-const spacedSentence = "this  has   many   spaces"
+const spacedSentence = "this  has   many   spaces";
+const multilineSentence = "this is the first line\nThis is the second line\r\n  This is the 3rd line";
 
-describe('findEndOfWord', () => {
+describe('findEndOfWordForward', () => {
 
     it('should return the same index when there are no more words', () => {
-        assert.equal(findEndOfWord(emptySentence, 0), 0);
-        assert.equal(findEndOfWord(exampleSentence, exampleSentence.length), exampleSentence.length);
+        assert.equal(findEndOfWordForward(emptySentence, 0), 0);
+        assert.equal(findEndOfWordForward(exampleSentence, exampleSentence.length), exampleSentence.length);
     });
 
     it('should return the index at the end of the word', () => {
-        assert.equal(findEndOfWord(exampleSentence, 0), 2);
-        assert.equal(findEndOfWord(exampleSentence, 1), 2);
-        assert.equal(findEndOfWord(exampleSentence, 2), 8);
-        assert.equal(findEndOfWord(exampleSentence, 3), 8);
-        assert.equal(findEndOfWord(exampleSentence, 4), 8);
-        assert.equal(findEndOfWord(exampleSentence, 5), 8);
-        assert.equal(findEndOfWord(exampleSentence, 6), 8);
-        assert.equal(findEndOfWord(exampleSentence, 7), 8);
-        assert.equal(findEndOfWord(exampleSentence, 8), 14);
+        assert.equal(findEndOfWordForward(exampleSentence, 0), 2);
+        assert.equal(findEndOfWordForward(exampleSentence, 1), 2);
+        assert.equal(findEndOfWordForward(exampleSentence, 2), 8);
+        assert.equal(findEndOfWordForward(exampleSentence, 3), 8);
+        assert.equal(findEndOfWordForward(exampleSentence, 4), 8);
+        assert.equal(findEndOfWordForward(exampleSentence, 5), 8);
+        assert.equal(findEndOfWordForward(exampleSentence, 6), 8);
+        assert.equal(findEndOfWordForward(exampleSentence, 7), 8);
+        assert.equal(findEndOfWordForward(exampleSentence, 8), 14);
     });
 
     it('should return the index at the end of the next word when there are many spaces between words', () => {
-        assert.equal(findEndOfWord(spacedSentence, 3), 8);
-        assert.equal(findEndOfWord(spacedSentence, 4), 8);
-        assert.equal(findEndOfWord(spacedSentence, 5), 8);
+        assert.equal(findEndOfWordForward(spacedSentence, 3), 8);
+        assert.equal(findEndOfWordForward(spacedSentence, 4), 8);
+        assert.equal(findEndOfWordForward(spacedSentence, 5), 8);
     });
+
+    it('should return the correct index when going to a word on a new line', () => {
+        assert.equal(findEndOfWordForward(multilineSentence, 20), 21);
+        assert.equal(findEndOfWordForward(multilineSentence, 21), 26);
+        assert.equal(findEndOfWordForward(multilineSentence, 44), 45);
+        assert.equal(findEndOfWordForward(multilineSentence, 45), 53);
+    })
 
 });
